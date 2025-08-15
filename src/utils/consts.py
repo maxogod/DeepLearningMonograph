96b857308fadb1bb3d8bfbd3ae66942a3ff14097
maxogod <maxi18wars@gmail.com>
2025-08-15 19:25:22 -0300

    unet 3D model

--- src/utils/consts.py
+++ src/utils/consts.py
@@ -4,6 +4,9 @@ TRAINING_DATA_PATH = "../data/raw/brats_nii/training/"
 PREPROC_DATA_PATH = "../data/preprocessed/brats_nii/"
 SPLIT_PREPROC_DATA_PATH = "../data/preprocessed/split_brats_nii/"
 
+VOLUMES_PATH = "/volumes"
+MASKS_PATH = "/masks"
+
 # Image sequence suffixes
 T1CE_SUFFIX = "_t1ce.nii"
 T2_SUFFIX = "_t2.nii"

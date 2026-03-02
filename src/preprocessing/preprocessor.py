c618ad6e0d7e24957216153fb9cc237a1db5841c
maxogod <maxi18wars@gmail.com>
2026-03-02 20:10:46 -0300

    feat: new preprocessor class

---
+++ src/preprocessing/preprocessor.py
@@ -0,0 +1,13 @@
+from glob import glob
+import logging
+from src.utils.consts import T1CE_SUFFIX, T2_SUFFIX, FLAIR_SUFFIX, SEG_SUFFIX
+
+logger = logging.getLogger(__name__)
+
+class Preprocessor:
+    def __init__(self, data_path: str) -> None:
+        self.t1ce_files = sorted(glob(data_path + "*/*" + T1CE_SUFFIX))
+        self.t2_files = sorted(glob(data_path + "*/*" + T2_SUFFIX))
+        self.flair_files = sorted(glob(data_path + "*/*" + FLAIR_SUFFIX))
+        self.seg_files = sorted(glob(data_path + "*/*" + SEG_SUFFIX))
+        logger.debug(f"Found {len(self.t1ce_files)} T1CE files, {len(self.t2_files)} T2 files, {len(self.flair_files)} FLAIR files, and {len(self.seg_files)} SEG files.")

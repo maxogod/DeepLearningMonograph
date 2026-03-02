d7fb4c36d32ee839ae95405c8bc78cdbfc1a12fb
maxogod <maxi18wars@gmail.com>
2026-03-02 20:11:02 -0300

    feat: add new config and logger

---
+++ src/config/config.py
@@ -0,0 +1,13 @@
+from enum import Enum
+import os
+
+class Environment(Enum):
+    DEVELOPMENT = "development"
+    PRODUCTION = "production"
+
+class Config:
+    def __init__(self):
+        env = os.environ.get("ENVIRONMENT")
+        self.environment = Environment.DEVELOPMENT
+        if env == "production":
+            self.environment = Environment.PRODUCTION
---
+++ src/utils/logger.py
@@ -0,0 +1,33 @@
+import logging
+import logging.config
+from src.config.config import Environment
+
+LOGGING_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {
+            "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "formatter": "standard",
+        },
+    },
+    "root": {
+        "handlers": ["console"],
+    },
+}
+
+
+def setup_logging(env: Environment):
+    config = LOGGING_CONFIG.copy()
+
+    level = "DEBUG" if env == Environment.DEVELOPMENT else "INFO"
+
+    config["handlers"]["console"]["level"] = level
+    config["root"]["level"] = level
+
+    logging.config.dictConfig(config)

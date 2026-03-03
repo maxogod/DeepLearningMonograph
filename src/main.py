53995b940f55f3b4a1bcf86d8fe43b2038c03928
maxogod <maxi18wars@gmail.com>
2026-03-03 12:49:15 -0300

    feat: preprocessing finished

--- src/main.py
+++ src/main.py
@@ -5,12 +5,14 @@ from src.config.config import Config
 from src.preprocessing.preprocessor import Preprocessor
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python main.py <data_path>")
+    if len(sys.argv) < 3:
+        print("Usage: python -m src.main <data_path> <save_path>")
         sys.exit(1)
 
     config = Config()
     logger.setup_logging(config.environment)
 
     data_path = sys.argv[1]
+    save_path = sys.argv[2]
     preprocessor = Preprocessor(data_path)
+    preprocessor.preprocess(save_path)
--- src/preprocessing/preprocessor.py
+++ src/preprocessing/preprocessor.py
@@ -1,5 +1,16 @@
 from glob import glob
-from src.utils.consts import FLAIR_SUFFIX, SEG_SUFFIX, T1CE_SUFFIX, T2_SUFFIX
+import numpy as np
+from sklearn.preprocessing import MinMaxScaler
+import torch
+import torch.nn.functional as F
+from src.utils import file_operations
+from src.utils.consts import (
+    FLAIR_SUFFIX,
+    SEG_SUFFIX,
+    T1CE_SUFFIX,
+    T2_SUFFIX,
+    VOLUME_SUFFIX,
+)
 from src.utils.logger import get_logger
 
 
@@ -15,3 +26,41 @@ class Preprocessor:
         logger.debug(
             f"Found {len(self.t1ce_files)} T1CE files, {len(self.t2_files)} T2 files, {len(self.flair_files)} FLAIR files, and {len(self.seg_files)} SEG files."
         )
+
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
+
+    def preprocess(self, save_path: str):
+        count = 0
+        for t1ce_file, t2_file, flair_file, seg_file in zip(
+            self.t1ce_files, self.t2_files, self.flair_files, self.seg_files
+        ):
+            t1ce_img = self.scale_volume(file_operations.load_nii(t1ce_file)).astype(
+                np.float32
+            )
+            t2_img = self.scale_volume(file_operations.load_nii(t2_file)).astype(
+                np.float32
+            )
+            flair_img = self.scale_volume(file_operations.load_nii(flair_file)).astype(
+                np.float32
+            )
+            volume = np.stack([t1ce_img, t2_img, flair_img], axis=3)
+            volume = volume[56:184, 56:184, 13:141]  # Crop to 128x128x128
+
+            seg_img = file_operations.load_nii(seg_file).astype(np.uint8)
+            seg_img = seg_img[56:184, 56:184, 13:141]  # Crop to 128x128x128
+            seg_img[seg_img == 4] = 3
+            seg_img = (
+                F.one_hot(torch.from_numpy(seg_img), num_classes=4)
+                .numpy()
+                .astype(np.uint8)
+            )
+
+            np.save(f"{save_path}/{count}{VOLUME_SUFFIX}", volume)
+            count += 1
+
+    def scale_volume(self, volume: np.ndarray) -> np.ndarray:
+        original_shape = volume.shape
+        scaled_volume = self.scaler.fit_transform(
+            volume.reshape(-1, original_shape[-1])
+        ).reshape(original_shape)
+        return scaled_volume
